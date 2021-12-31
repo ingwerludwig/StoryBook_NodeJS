fd68bba8f8f82356e7f404d111a7fd9e6e5b70ba
Ingwer Ludwig <ingwerflash@gmail.com>
2021-12-31 08:00:45 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,23 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 const Story = require('../models/Story')
 
-router.get('/', ensureGuest,  (req, res) => {
-    res.render('login', {
-        layout: 'login',
-    })
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
 })
+
 router.get('/dashboard', ensureAuth, async (req, res) => {
-    try {
-        const stories = await Story.find({ user: req.user.id }).lean() //lean akan lebih cepat mencari karena lean tidak memeriksa dengan mengkonvert menjadi mongoose document terlebih dahulu
-        res.render('dashboard', {
-            name: req.user.firstName,
-            stories
-        })
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
-    }
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 module.exports = router
