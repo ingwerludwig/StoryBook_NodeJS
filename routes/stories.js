db0d349e7dbffcee5f74abed287a13f0ac42011d
Ingwer Ludwig <ingwerflash@gmail.com>
2021-12-31 08:05:43 +0700

    Update stories.js

--- routes/stories.js
+++ routes/stories.js
@@ -4,40 +4,42 @@ const { ensureAuth } = require('../middleware/auth')
 
 const Story = require('../models/Story')
 
-router.get('/add', ensureAuth,  (req, res) => {
-    res.render('stories/add')
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add')
 })
 
-router.post('/', ensureAuth,  async(req, res) => {
-    try {
-        req.body.user = req.user.id
-        await Story.create(req.body)
-        res.redirect('/dashboard')
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
-    }
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id
+    await Story.create(req.body)
+    res.redirect('/dashboard')
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
-router.get('/', ensureAuth, async(req, res) => {
-    try {
-        const stories = await Story.find({ status: 'public' })
-            .populate('user')
-            .sort({ createdAt: 'desc' })
-            .lean()
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
 
-        res.render('stories/index', {
-            stories,
-        })
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
-    }
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 router.get('/:id', ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).populate('user').lean()
+    //lean digunakan saat return data yg didapatkan yidak diubah lagi
+    //Sehingga bisa lebih cepat karna return yang diberikan berbebntuk POJO
 
     if (!story) {
       return res.render('error/404')
@@ -57,28 +59,28 @@ router.get('/:id', ensureAuth, async (req, res) => {
 })
 
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    try {
-      const story = await Story.findOne({
-        _id: req.params.id,
-      }).lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user != req.user.id) {
-        res.redirect('/stories')
-      } else {
-        res.render('stories/edit', {
-          story,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean()
+
+    if (!story) {
+      return res.render('error/404')
     }
-  })
-  
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      res.render('stories/edit', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
+
 router.put('/:id', ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).lean()
